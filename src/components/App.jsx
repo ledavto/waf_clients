5c9900a1ada5e8a599a78281a5402bce9f97f295
ledavto <ser_ega@ukr.net>
2025-01-17 23:03:10 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { UserForm } from './UserForm';
 import { UserList } from './UserList';
 import { nanoid } from 'nanoid';
