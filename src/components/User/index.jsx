08beabb411ebf65571182a1cd2310c3bb9a44345
ledavto <ser_ega@ukr.net>
2025-01-17 22:59:36 +0200

    fix

--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
 export const User = ({ addUser }) => {
   //   const [name, setName] = useState('');
--- src/components/UserForm/index.jsx
+++ src/components/UserForm/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
 export const UserForm = ({ addUser }) => {
   // const [name, setName] = useState('');
